cf2385b54c848513280a93ac52a799bee7896c2b
Amitesh Jammula <amiteshjammula@Amiteshs-MacBook-Pro.local>
2025-10-13 16:02:03 +0530

    8k port test

--- backend/main.py
+++ backend/main.py
@@ -1296,7 +1296,7 @@ if __name__ == "__main__":
     uvicorn.run(
         "main:app", 
         host="127.0.0.1", 
-        port=80, 
+        port=8000, 
         reload=True, 
         workers=1,
         log_level="info"
